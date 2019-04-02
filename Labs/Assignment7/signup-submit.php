621a2905dddac0427dc87f8595b07f504c536968
Joseph M Dunton <jmdunton@sjobs.muad.local>
2019-04-02 14:29:20 -0400

    testing signup-submit.php

--- Labs/Assignment7/signup-submit.php
+++ Labs/Assignment7/signup-submit.php
@@ -1,12 +1,12 @@
 <?php include("top.html"); ?>
 
 <div>
-	name: <?= echo $_POST['name'] . "<br />" ?>
-	gender: <?= echo $_POST['gender'] . "<br />" ?>
-	minAge: <?= echo $_POST['minAge'] . "<br />" ?>
-	maxAge: <?= echo $_POST['maxAge'] . "<br />" ?>
-	os: <?= echo $_POST['os'] . "<br />" ?>
-	ptype: <?= echo $_POST['ptype'] . "<br />" ?>
+	name: <?= echo ($_POST['name'] . "<br />"); ?>
+	gender: <?= echo ($_POST['gender'] . "<br />"); ?>
+	minAge: <?= echo ($_POST['minAge'] . "<br />"); ?>
+	maxAge: <?= echo ($_POST['maxAge'] . "<br />"); ?>
+	os: <?= echo ($_POST['os'] . "<br />"); ?>
+	ptype: <?= (echo $_POST['ptype'] . "<br />"); ?>
 </div>
 
 <?php include("bottom.html"); ?>
