3686c0c0cc6fbbe8887183e3727aa2fc7b4b6142
Joseph M Dunton <jmdunton@sjobs.muad.local>
2019-04-02 14:20:43 -0400

    working on signup-submit.php

---
+++ Labs/Assignment7/signup-submit.php
@@ -0,0 +1,5 @@
+<?php include("top.html"); ?>
+
+
+
+<?php include("bottom.html"); ?>
--- Labs/Assignment7/signup.php
+++ Labs/Assignment7/signup.php
@@ -1,12 +1,12 @@
 <?php include("top.html"); ?>
 
 <div>
-	<form >
+	<form action="/signup-submit.php" method="post">
 		<fieldset>
 		Name: <input type="text" name="name" size="16" maxlength="16" /> <br />
 		Gender: <input type="radio" name="gender" value="Male" /> <label for="Male">Male</label> <input type="radio" name="gender" value="Female" /> <label for="Female">Female</label> <br />
 		Age: <input type="text" name="age" size="6" maxlength="2" /> <br />
-		Personality Type: <input type="test" name="ptype" size="6" maxlength="4" /> (<a href="http://www.humanmetrics.com/cgi-win/JTypes2.asp"> Don't know your type?</a>) <br />
+		Personality Type: <input type="test" name="ptype" size="6" maxlength="4" /> (<a href="http://www.humanmetrics.com/cgi-win/JTypes2.asp">Don't know your type?</a>) <br />
 		Favorite OS: <select required>
 						<option value="windows">Windows</option>
 						<option value="macOS">MacOS</option>
