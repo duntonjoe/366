6bd8d08c4174d093f767cdff0ef67ca10f05bb2c
Joseph M Dunton <jmdunton@sjobs.muad.local>
2019-04-02 13:58:19 -0400

    input field min lengths

--- Labs/Assignment7/signup.php
+++ Labs/Assignment7/signup.php
@@ -2,16 +2,16 @@
 
 <div>
 	<form>
-		Name: <input type="text" name="name" size="16" /> <br />
+		Name: <input type="text" name="name" size="16" maxlength="16" /> <br />
 		Gender: <input type="radio" name="gender" value="Male" /> <label for="Male">Male</label> <input type="radio" name="gender" value="Female" /> <label for="Female">Female</label> <br />
-		Age: <input type="text" name="age" /> <br />
+		Age: <input type="text" name="age" size="6" maxlength="2" /> <br />
 		Personality Type: <input type="test" name="ptype" size="6" /> (<a href="http://www.humanmetrics.com/cgi-win/JTypes2.asp"> Don't know your type?</a>) <br />
-		Favorite OS: <select>
+		Favorite OS: <select required>
 						<option value="windows">Windows</option>
 						<option value="macOS">MacOS</option>
 						<option value="linux">Linux</option>
 					</select>
-		Seeking Age: <input type="text" name="minAge" size="6" /> to <input type="text" name="maxAge" size="6" /> <br />
+		Seeking Age: <input type="text" name="minAge" size="6" maxlength="2" /> to <input type="text" name="maxAge" size="6" maxlength="2" /> <br />
 		<input type="submit" name="submit" value="Sign Up" />
 	</form>
 </div>
