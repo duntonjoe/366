1f07e54b9e0f9b95724bf04e9dc61ddd1fb20063
Joseph M Dunton <jmdunton@sjobs.muad.local>
2019-04-02 13:42:28 -0400

    working on signup page

---
+++ Labs/Assignment7/signup.php
@@ -0,0 +1,17 @@
+<?php include("top.html"); ?>
+
+<div>
+	<form>
+		Name: <input type="text" name="name" size="16" /> <br />
+		Gender: <input type="radio" name="gender" value="Male" /> <input type="radio" name="gender" value="Female" /> <br />
+		Age: <input type="text" name="age" /> <br />
+		Personality Type: <input type="test" name="ptype" size="6" /> (<a href="http://www.humanmetrics.com/cgi-win/JTypes2.asp"> Don't know your type?</a>) <br />
+		Favorite OS: <select>
+						<option value="windows">Windows</option>
+						<option value="macOS">MacOS</option>
+						<option value="linux">Linux</option>
+					</select>
+		Seeking Age: <input type="text" name="minAge" size="6" /> to <input type="text" name="maxAge" size="6" /> <br />
+		<input type="submit" name="submit" value="Sign Up" />
+	</form>
+</div>
