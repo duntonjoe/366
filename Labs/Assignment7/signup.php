b64d50adaf2fd0b2a6e4e56200b1a4a70967163c
Joseph M Dunton <jmdunton@sjobs.muad.local>
2019-04-02 13:48:21 -0400

    fixing radio button labels

--- Labs/Assignment7/signup.php
+++ Labs/Assignment7/signup.php
@@ -3,6 +3,8 @@
 <div>
 	<form>
 		Name: <input type="text" name="name" size="16" /> <br />
+				<label for="Male">Male</label>
+				<label for="Female">Female</label>
 		Gender: <input type="radio" name="gender" value="Male" /> <input type="radio" name="gender" value="Female" /> <br />
 		Age: <input type="text" name="age" /> <br />
 		Personality Type: <input type="test" name="ptype" size="6" /> (<a href="http://www.humanmetrics.com/cgi-win/JTypes2.asp"> Don't know your type?</a>) <br />
