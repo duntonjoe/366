038b5b71703f28fcc261c41674468f63bd31e836
Joseph M Dunton <jmdunton@sjobs.muad.local>
2019-04-08 17:14:21 -0400

    working on matches-submit.php

--- Labs/Assignment7/matches.php
+++ Labs/Assignment7/matches.php
@@ -1,12 +1,15 @@
 <?php include("top.html"); ?>
 
 <div>
-	<fieldset>
-		<legend>Returning User:</legend>
-		<ul>
-			<li><div class="column">Name:</div> <input type="text" name="name" size="16" maxlength="16" /> </li></ul>
-		<input type="submit" name="submit" value="View My Matches" />
-	</fieldset>
+	<form action="matches-submit.php" method="GET">
+		<fieldset>
+			<legend>Returning User:</legend>
+			<ul>
+				<li> <div class="column">Name:</div> <input type="text" name="name" size="16" maxlength="16" /> </li>
+			</ul>
+			<input type="submit" name="submit" value="View My Matches" />
+		</fieldset>
+	</form>
 </div>
 
 <?php include("bottom.html"); ?>
