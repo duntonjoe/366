3dc00da41dcd2eb92fa35c699c7fc46ca046041c
Joseph M Dunton <jmdunton@sjobs.muad.local>
2019-04-08 15:05:34 -0400

    fixing signup-submit.php

--- Labs/Assignment7/signup-submit.php
+++ Labs/Assignment7/signup-submit.php
@@ -1,7 +1,15 @@
 <?php include("top.html"); ?>
 
-<?php $dataString = $_POST['name'].",".$_POST['gender'].",".$_POST['age'].",".$_POST['ptype'].",".$_POST['os'].",".$_POST['minAge'].",".$_POST['maxAge']; ?>
+<?php $dataString = $_POST['name'].",".$_POST['gender'].",".$_POST['age'].",".$_POST['ptype'].",".$_POST['os'].",".$_POST['minAge'].",".$_POST['maxAge']; 
+		$singles = 'singles.txt';
+		file_put_contents($singles, $dataString, FILE_APPEND);
+?>
 
-<h1><?= $dataString ?></h1>
+<h1>Thank You!<h1> <br />
+<br />
+<p>Welcome to NerdLuv, <?= $_POST['name']; ?></p> <br />
+<br />
+<p>Now <a href="matches.php"></a>log in to see your matches!</p>
+
 
 <?php include("bottom.html"); ?>
