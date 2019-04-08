e1620b732f57e251dd0840a13383bafe7a1362e3
Joseph M Dunton <jmdunton@sjobs.muad.local>
2019-04-08 12:41:01 -0400

    fixing formatting

--- Labs/Assignment7/matches.php
+++ Labs/Assignment7/matches.php
@@ -1,9 +1,9 @@
 <?php include("top.html"); ?>
 
 <div>
-	<fieldset>
+	<fieldset class="forms">
 		<legend>Returning User:</legend>
-		Name: <input type="text" name="name" size="16" maxlength="16" /> <br />
+		<div class="forms">Name:</div> <input type="text" name="name" size="16" maxlength="16" /> <br />
 		<input type="submit" name="submit" value="View My Matches" />
 	</fieldset>
 </div>
--- Labs/Assignment7/signup.php
+++ Labs/Assignment7/signup.php
@@ -4,7 +4,7 @@
 	<form action="signup-submit.php" method="post">
 		<fieldset>
 		<legend>New User Signup:</legend>
-		Name: <input type="text" name="name" size="16" maxlength="16" class = "forms"/> <br />
+		<div class="forms"Name:</div> <input type="text" name="name" size="16" maxlength="16"/> <br />
 		Gender: <input type="radio" name="gender" value="Male" /> <label for="Male">Male</label> <input type="radio" name="gender" value="Female" class = "forms"/> <label for="Female">Female</label> <br />
 		Age: <input type="text" name="age" size="6" maxlength="2" class = "forms"/> <br />
 		Personality Type: <input type="test" name="ptype" size="6" maxlength="4" class = "forms"/> (<a href="http://www.humanmetrics.com/cgi-win/JTypes2.asp">Don't know your type?</a>) <br />
