46f6498addf78fdca7dabe7546e656f1d9925ede
Joseph M Dunton <jmdunton@sjobs.muad.local>
2019-04-08 13:20:27 -0400

    fixing post stuff

--- Labs/Assignment7/signup-submit.php
+++ Labs/Assignment7/signup-submit.php
@@ -1,8 +1,11 @@
 <?php include("top.html"); ?>
 
-<?php $name = $_POST['name']; ?>
-
-
-<h1><?= $name ?></h1>
+<h1><?= $_POST['name']; ?></h1></br	>
+<h1><?= $_POST['age']; ?></h1></br	>
+<h1><?= $_POST['gender']; ?></h1></br>
+<h1><?= $_POST['minAge']; ?></h1></br>
+<h1><?= $_POST['maxAge']; ?></h1></br>
+<h1><?= $_POST['os']; ?></h1></br>
+<h1><?= $_POST['ptype']; ?></h1></br	>
 
 <?php include("bottom.html"); ?>
