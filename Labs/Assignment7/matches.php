36a885327fd7f29680bfe00c4d2317629fe310ea
Joseph M Dunton <jmdunton@sjobs.muad.local>
2019-04-08 12:27:11 -0400

    added check matches page

---
+++ Labs/Assignment7/matches.php
@@ -0,0 +1,11 @@
+<?php include("top.html"); ?>
+
+<div>
+	<fieldset>
+		<legend>Returning User:</legend>
+		Name: <input type="text" name="name" size="16" maxlength="16" /> <br />
+		<input type="submit" name="submit" value="View My Matches" />
+	</fieldset>
+</div>
+
+<?php include("bottom.html"); ?>
