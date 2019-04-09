adb1a15c022903884801f545ab3511cf014f636e
Joseph M Dunton <jmdunton@sjobs.muad.local>
2019-04-09 13:21:42 -0400

    gettinng matches-submit to work

--- Labs/Assignment7/matches-submit.php
+++ Labs/Assignment7/matches-submit.php
@@ -1,6 +1,19 @@
 <?php include("top.html"); ?>
 
-<?php $loginName = $_GET['name']; ?>
-<h1><?= $loginName ?></h1>
+<?php 
+	$loginName = $_GET['name']; 
+	$single = fopen('singles.txt');
+	$targetUser = 0;
+	$currentUser = array();
+	for each($single as $user){
+		$currentUser[] = explode(",", $user);
+		if($currentUser[0] == $loginName){
+			$targetUser[] = $currentUser;
+		}
+	}
+?>
+
+<h1><?= $targetUser[0];?></h1>
+
 
 <?php include("bottom.html"); ?>
