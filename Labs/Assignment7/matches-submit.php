b64dea5e1b140325998c2b74a9677ae62dee8e16
Joseph M Dunton <jmdunton@sjobs.muad.local>
2019-04-09 17:46:04 -0400

    gettinng matches-submit to work

--- Labs/Assignment7/matches-submit.php
+++ Labs/Assignment7/matches-submit.php
@@ -1,6 +1,26 @@
 <?php include("top.html"); ?>
 
 <?php 
+		function pcheck($a , $b){
+			for($i = 0; $i < 3; $i++){
+				if($a[$i] == $b[$i]){
+					return true;
+				}
+			}
+			return false;
+		}
+
+		function ageCheck ($lower, $upper, $age){
+			if($age >= $lower && $age <= $upper){
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+?>
+
+<?php 
 	$loginName = $_GET['name']; 
 	$single = file('singles.txt');
 	$targetUser;
@@ -10,6 +30,13 @@
 		if($currentUser[0] == $loginName){
 			$targetUser = $currentUser;
 			break;
+		}
+	}
+	foreach($single as $user){
+		$currentUser =  explode(",", $user);
+		$ageCheck = ageCheck($currentUser[5], $currentUser[6], $targetUser[1]) && ageCheck($targetUser[5], $targerUser[6], $currentUser[1]);
+		if($currentUser[1] != $targetUser[1] && $currentUser[4] == $targetUser[4] && pcheck($currentUser[3], $targetUser[3]) && $ageCheck){
+			print_r($currentUser);
 		}
 	}
 ?>
