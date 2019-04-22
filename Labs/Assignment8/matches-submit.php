2ea18350090b735221f4868c85e46f9e89bcac7a
Joseph M Dunton <jmdunton@sjobs.muad.local>
2019-04-22 19:04:19 -0400

    migrating forms to web service

--- Labs/Assignment8/matches-submit.php
+++ Labs/Assignment8/matches-submit.php
@@ -1,64 +1,34 @@
-<?php include("top.html"); ?>
+<?php include("top.html");
 
-<?php 
-		function pcheck($a , $b){
-			for($i = 0; $i < 4; $i++){
-				if($a[$i] == $b[$i]){
-					return true;
-				}
-			}
-			return false;
-		}
+	$url = "http://".$_SERVER['HTTP_HOST']."/366/Labs/Assignment8/nerdluv.php";
+	$name = $_GET['name'];
 
-		function ageCheck ($lower, $upper, $age){
-			if($age >= $lower && $age <= $upper){
-				return true;
-			}
-			else{
-				return false;
-			}
-		}
-?>
+	$matches = http_build_query(array('name' => $name));
 
-<?php 
-	$loginName = $_GET['name']; 
-	$single = file('singles.txt');
-	$targetUser;
-	$currentUser;
-	foreach($single as $user){
-		$currentUser =  explode(",", $user);
-		if($currentUser[0] == $loginName){
-			$targetUser = $currentUser;
-			break;
-		}
+	$jsonDATA = json_decode(file_get_contents($url.$matches));
+
+	foreach($jsonData[data] as $line){
+		print_r($line);
 	}
-	?>
 
-	<strong>Matches for <?= $targetUser[0] ?></strong>
-	<?php
+?>
+	<strong>Matches for <?= $name ?></strong>
 
-	foreach($single as $user){
-		$currentUser =  explode(",", $user);
-		$ageCheck = ageCheck($currentUser[5], $currentUser[6], $targetUser[2]) && ageCheck($targetUser[5], $targetUser[6], $currentUser[2]);
-
-		if($currentUser[1] != $targetUser[1] && $currentUser[4] == $targetUser[4] && pcheck($currentUser[3], $targetUser[3]) && $ageCheck){
-			?>
+<?php 
+	foreach($jsonData[data] as $line){
+?>
 
 			<div class="match">
-				<p><img src="user.jpg" alt="user image"> <?= $currentUser[0]; ?></p>
+				<p><img src="user.jpg" alt="user image"> <?= $line['name']; ?></p>
 				<ul>
-					<li><strong>gender:</strong> <?= $currentUser[1]; ?> </li>
-					<li><strong>age:</strong> <?= $currentUser[2]; ?> </li>
-					<li><strong>type:</strong> <?= $currentUser[3]; ?> </li>
-					<li><strong>OS:</strong> <?= $currentUser[4]; ?> </li>
+					<li><strong>gender:</strong> <?= $line['gender']; ?> </li>
+					<li><strong>age:</strong> <?= $line['age']; ?> </li>
+					<li><strong>type:</strong> <?= $line['ptype']; ?> </li>
+					<li><strong>OS:</strong> <?= $line['os']; ?> </li>
 				</ul>
 			</div>
+<?php
 
-
-			<?php
-		}
 	}
-?>
 
-
-<?php include("bottom.html"); ?>
+include("bottom.html"); ?>
