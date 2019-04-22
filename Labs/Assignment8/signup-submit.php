e89a5a19a1114426983671eb55b98c8182620ccd
Joseph M Dunton <jmdunton@sjobs.muad.local>
2019-04-22 15:37:26 -0400

    migrating forms to web service

--- Labs/Assignment8/signup-submit.php
+++ Labs/Assignment8/signup-submit.php
@@ -1,9 +1,28 @@
 <?php include("top.html"); ?>
 
-<?php $dataString = "\n".$_POST['name'].",".$_POST['gender'].",".$_POST['age'].",".$_POST['ptype'].",".$_POST['os'].",".$_POST['minAge'].",".$_POST['maxAge']; 
-		$singles = 'singles.txt';
-		print_r($dataString);
-		file_put_contents($singles, $dataString, FILE_APPEND);
+
+<?php
+	$url = "http://".$_SERVER['HTTP_HOST']."/Lab8/nerdluv.php";
+	$postParameters = http_build_query(
+		array(
+			'name' = $_POST[0],
+			'gender' = $_POST[1],
+			'age' = $_POST[2],
+			'ptype' = $_POST[3],
+			'os' = $_POST[4],
+			'minAge' = $_POST[5],
+			'maxAge' = $_POST[6]
+		)
+	);
+	$opts = array('http' =>
+	    array(
+	        'method'  => 'POST',
+	        'header'  => 'Content-type: application/x-www-form-urlencoded',
+	        'content' => $postParameters
+	    )
+	);
+	$context = stream_context_create($opts);
+	$result = file_get_contents($url, false, $context);
 ?>
 
 <h1>Thank You!</h1> <br />
