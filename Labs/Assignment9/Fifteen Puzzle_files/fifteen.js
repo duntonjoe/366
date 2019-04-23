e17255137482b2e930065a1eb2c764c1f16e38e3
Joseph M Dunton <jmdunton@sjobs.muad.local>
2019-04-23 17:20:39 -0400

    still working on lab

--- Labs/Assignment9/Fifteen Puzzle_files/fifteen.js
+++ Labs/Assignment9/Fifteen Puzzle_files/fifteen.js
@@ -1,1 +1,11 @@
-/** use nth (selects the nth element of a div )
+
+/** populate pieces with correct background segments **/
+
+$( document ).ready(function() {
+    var i;
+    var targetPiece;
+	for( i = 0; i <= 15; i++;){
+		targetPiece = $("#puzzlearea:nth-child(i)");
+		targetPiece.style.backgroundImage = "url(./friend.jpg)";
+	}
+});
