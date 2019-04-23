6521138456ddac5a2f23b24576134b8fdb4d741a
JoeDunton <duntonjoe@gmail.com>
2019-04-22 23:11:38 -0400

    Update matches-submit.php
    12345

--- Labs/Assignment8/matches-submit.php
+++ Labs/Assignment8/matches-submit.php
@@ -3,7 +3,7 @@
 	$name = $_GET['name'];
 	$matches = http_build_query(array('name' => $name));
 	$jsonData = json_decode(file_get_contents($url.$matches), true);
-	print(json_encode($jsonData));
+	print(file_get_contents($url.$matches));
 ?>
 	<strong>Matches for <?= $name ?></strong>
 
