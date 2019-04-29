419482cd8ba5396f74c2bcf79670acd94da4996e
Joseph M Dunton <jmdunton@agamemnon.muad.local>
2019-04-29 19:57:07 -0400

    pieces are now indexed

--- Labs/Assignment9/Fifteen Puzzle_files/fifteen.js
+++ Labs/Assignment9/Fifteen Puzzle_files/fifteen.js
@@ -1,9 +1,15 @@
 var puzzle = [[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16]];
 
-$("#puzzlearea > div").click(
+$(document).ready(function() {
 
-	function() {
-		var currPieceNumber = $(this).index();
-		console.log(currPieceNumber);
+$("#puzzlearea > div").click(function() {
+		var currPiece = $(this);
+		var currPieceNumber = ($("#puzzlearea > div").index(this)) + 1;
+		
+		console.log("Piece #" + currPieceNumber + " clicked at offset: ( X: " + currPiecePosX + " Y:" + currPiecePosY + " )");
+
+
 	}
 );
+
+});
