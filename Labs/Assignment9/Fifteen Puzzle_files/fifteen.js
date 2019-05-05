2053c40654c48a7a94859f20be3e60be6b96e37f
JoeDunton <duntonjoe@gmail.com>
2019-05-05 15:04:35 -0400

    Update fifteen.js
    fixed comments and what not.

--- Labs/Assignment9/Fifteen Puzzle_files/fifteen.js
+++ Labs/Assignment9/Fifteen Puzzle_files/fifteen.js
@@ -1,10 +1,17 @@
+/**
+*	Assignment: 	Create a browser based 15 puzzle utilizing jquery and javascript functionality.
+**/
+
+
+"use strict";
+
 var piece16top = 300;
 var piece16left = 300;
 var currPieceCoordinates = [];
 
 /** 
-this function takes care of swapping a piece with the blank square.
-it assumes the swap is valid.
+*	this function takes care of swapping a piece with the blank square.
+*	it assumes the swap is valid.
 **/
 function swapWith16(target){
 	var targetTop = target.position().top;
@@ -15,19 +22,16 @@ function swapWith16(target){
 
 }
 
-
-
-
 $(document).ready(function() {
-
 	$("#puzzlearea > div").click(function() {
 			var currPiece = $(this);
 			var currPieceNumber = ($("#puzzlearea > div").index(this)) + 1;
 			var currPiecePos = currPiece.position();
 
-			
-			console.log("Piece #" + currPieceNumber + " clicked at offset: ( TOP: " + currPiecePos.top + " LEFT:" + currPiecePos.left + " ) ");
-
+			/**
+			*	this is the logic for piece movement. 
+			*	Javascript has no logical XOR, so I have to do this garbage to make diagonal movement illegal.
+			**/
 			if((Math.abs(currPiecePos.left - piece16left) == 100) && (Math.abs(currPiecePos.top - piece16top) != 100)){
 				swapWith16($(this));
 				return;
@@ -35,9 +39,14 @@ $(document).ready(function() {
 			else if ((Math.abs(currPiecePos.top - piece16top) == 100) && (Math.abs(currPiecePos.left - piece16left) != 100)){
 				swapWith16($(this));
 			}
-
 		});
 
+
+	/**
+	*	I've elected to shuffle by iterating though the puzzle 15 times.
+	*	In each iteration, I grab a random piece and swap it with the blank.
+	*	This seems psuedo-random enough for our purposes.
+	**/
 	$("#shufflebutton").click(function() {
        var board = $("#puzzlearea");
        var rand = 2;
